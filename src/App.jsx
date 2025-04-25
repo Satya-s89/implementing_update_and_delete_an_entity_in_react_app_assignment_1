cf0bbb9fa35375694800bd32dccc7bf260b6481d
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-25 12:11:16 +0530

    assignment completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import UpdateItem from "./components/UpdateItem";
+import { useState } from "react";
+// import axios from "axios";
+
 
 // use the following link to get the data
 // `/doors` will give you all the doors, to get a specific door use `/doors/1`.
@@ -6,7 +9,7 @@ const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
   // Get the existing item from the server
-  // const [item, setItem] = useState(null);
+  const [item, setItem] = useState(null);
   // pass the item to UpdateItem as a prop
 
   return <UpdateItem />;
